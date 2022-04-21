8a15a2717d8114e9adcb369b5fd9a95fed3c74d9
demidraksin <greatraksin@icloud.com>
2022-04-21 10:51:35 +0300

    AAdding lines

---
+++ 25_lesson_2104/00_figs.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+
+def lines(obj, lin):
+    length = 100
+    for i in range(lin):
+        obj.lt(90)
+        obj.fd(length)
+        obj.bk(length)
+        obj.rt(90)
+        obj.up()
+        obj.fd(10)
+        obj.down()
+
+
+def cross(obj, len):
+
+
+
+t = Turtle()
